7bdd04c4e2b65302f4c685a7f75fe4ac1eef51bc
Arthur <arthur@alternumerica.org>
2022-05-24 09:31:21 +0200

    corrections to settings

--- facilalingvo2/settings.py
+++ facilalingvo2/settings.py
@@ -19,21 +19,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w!9_wal*z&y!znncn!d(v%!n(as38c($tw2&sfrwur9x)i(nm*'
+SECRET_KEY = 'secretkey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 TEMPLATE_DEBUG = False
 
-ALLOWED_HOSTS = ['dico.facilalingvo.com',
-		'www.dico.facilalingvo.com',
-		'dico.facilalingvo.pw',
-		'www.dico.facilalingvo.pw',
-		'localhost','127.0.0.1',
-		'dico.facilalingvo.artnelson.webfactional.com',
-		'www.dico.facilalingvo.artnelson.webfactional.com',
-]
 
 
 # Application definition
@@ -186,13 +178,3 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = "mandatory"
-ACCOUNT_USERNAME_BLACKLIST =["admin","arcturus","arthuro"]
-
-
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'SSL0.OVH.NET'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'postmaster@facilalingvo.com'
-EMAIL_HOST_PASSWORD='v10pTGbkj5WFMvyfUZam'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- facilalingvo2/settings_prod.py
+++ facilalingvo2/settings_prod.py
@@ -2,3 +2,27 @@ from settings import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'w!9_wal*z&y!znncn!d(v%!n(as38c($tw2&sfrwur9x)i(nm*'
+
+
+ALLOWED_HOSTS = ['dico.facilalingvo.com',
+		'www.dico.facilalingvo.com',
+		'dico.facilalingvo.pw',
+		'www.dico.facilalingvo.pw',
+		'localhost','127.0.0.1',
+		'dico.facilalingvo.artnelson.webfactional.com',
+		'www.dico.facilalingvo.artnelson.webfactional.com',
+]
+
+ACCOUNT_USERNAME_BLACKLIST =["admin","arcturus","arthuro"]
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'SSL0.OVH.NET'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'postmaster@facilalingvo.com'
+EMAIL_HOST_PASSWORD='v10pTGbkj5WFMvyfUZam'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
